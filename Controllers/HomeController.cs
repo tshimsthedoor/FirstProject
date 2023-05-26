691de38563fea1846f18506e27eff8fbb4e4919c
tresor <tshims79@gmail.com>
2023-05-26 17:03:30 +0200

    Added the time date on my view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,8 @@ public class HomeController : Controller
 {
    public ViewResult Index()
     {
-        return View("MyView");
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView", viewModel);
     }
 }
